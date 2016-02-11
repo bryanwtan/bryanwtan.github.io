d7ab76a548accbba203c7204364f8d6fdb17e394
bryanwtan <bryan.w.tan@gmail.com>
2016-02-10 20:25:20 -0500

    let safari

--- splash.js
+++ splash.js
@@ -4,21 +4,21 @@
     window.setTimeout(fadeInNavigation, [200]);
 
     function fadeInNavigation() {
-        let nav = document.getElementById('custom-nav'), nstyle = nav.style;
+        var nav = document.getElementById('custom-nav'), nstyle = nav.style;
         nstyle.opacity = '1';
 
         window.setTimeout(fadeInHello, [500]);
     }
 
     function fadeInHello() {
-        let hello = document.getElementById('hello-text'), hstyle = hello.style;
+        var hello = document.getElementById('hello-text'), hstyle = hello.style;
         hstyle.opacity = '1';
 
         window.setTimeout(fadeInSplash, [500]);
     }
 
     function fadeInSplash() {
-        let profile = document.getElementById('splash'), pstyle = profile.style;
+        var profile = document.getElementById('splash'), pstyle = profile.style;
         pstyle.opacity = '1';
     }
 }());
