d18967c4510646ba73ca7f5e2e3ee449689cea53
bryanwtan <bryan.w.tan@gmail.com>
2016-02-10 20:17:58 -0500

    smooth loading

--- splash.js
+++ splash.js
@@ -1,12 +1,24 @@
 (function() {
     'use strict';
 
-    function slideOut() {
-        let hello = document.getElementById('hello-text'), hstyle = hello.style;
-        let profile = document.getElementById('splash'), pstyle = profile.style;
-        hstyle.opacity = '0';
-        pstyle.opacity = '0';
+    window.setTimeout(fadeInNavigation, [200]);
+
+    function fadeInNavigation() {
+        let nav = document.getElementById('custom-nav'), nstyle = nav.style;
+        nstyle.opacity = '1';
+
+        window.setTimeout(fadeInHello, [500]);
     }
 
-    let slideOutTimeout = window.setTimeout(slideOut, [1500]);
+    function fadeInHello() {
+        let hello = document.getElementById('hello-text'), hstyle = hello.style;
+        hstyle.opacity = '1';
+
+        window.setTimeout(fadeInSplash, [500]);
+    }
+
+    function fadeInSplash() {
+        let profile = document.getElementById('splash'), pstyle = profile.style;
+        pstyle.opacity = '1';
+    }
 }());
